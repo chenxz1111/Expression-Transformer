3c82a7ee8e3bb34c23b4568be52fd059d1af708b
chenxz <792146066@qq.com>
2021-12-15 14:40:46 +0800

    preprocess and dataloader

--- expression_loader.py
+++ expression_loader.py
@@ -1,19 +1,30 @@
 import numpy as np
 import os
 
-def ExpressionLoader(data):
-    path = os.path.join('data', 'bool_expr_data')
-    tot_voc = []
+def ExpressionLoader(data, Expression_type):
+    path = os.path.join('data', Expression_type)
+    expr_tot_voc = []
+    res_tot_voc = []
     file_list = os.listdir(path)
     for f_name in file_list:
-        f = os.path.join(path, f_name)
-        with open(f, 'r') as fp:
-            lines = fp.readlines()
-            for l in lines:
-                tot_voc += l.split()
-    voc_dic = sorted(list(set(tot_voc)))
-    voc_dic = ['ZERO'] + voc_dic
-    for i,x in enumerate(voc_dic): print(i,x)
+        if '_expr.txt' in f_name :
+            f = os.path.join(path, f_name)
+            with open(f, 'r') as fp:
+                lines = fp.readlines()
+                for l in lines:
+                    expr_tot_voc += l.split()
+        elif '_res.txt' in f_name :
+            f = os.path.join(path, f_name)
+            with open(f, 'r') as fp:
+                lines = fp.readlines()
+                for l in lines:
+                    res_tot_voc += l.split()
+    expr_voc_dic = sorted(list(set(expr_tot_voc)))
+    expr_voc_dic = ['ZERO'] + expr_voc_dic
+    res_voc_dic = sorted(list(set(res_tot_voc)))
+    res_voc_dic = ['ZERO'] + res_voc_dic
+    for i ,x in enumerate(expr_voc_dic): print(i,x)
+    for i ,x in enumerate(res_voc_dic): print(i,x)
     if data == 'train':
         expr_file  = os.path.join(path, 'train_expr.txt')
         res_file = os.path.join(path, 'train_res.txt')
@@ -24,24 +35,19 @@ def ExpressionLoader(data):
         raise Exception()
     expr_data = []
     res_data = []
-    max_len = 0
+    expr_max_len = 0
     with open(expr_file) as ef:
         lines = ef.readlines()
         for l in lines:
             vec = l.split()
-            max_len = max(max_len, len(vec))
-    with open(res_file) as rf:
-        lines = rf.readlines()
-        for l in lines:
-            vec = l.split()
-            max_len = max(max_len, len(vec))
+            expr_max_len = max(expr_max_len, len(vec))
     with open(expr_file) as ef:
         lines = ef.readlines()
         for l in lines:
             vec = l.split()
             tmp = []
             for w in vec:
-                tmp.append(voc_dic.index(w))
+                tmp.append(expr_voc_dic.index(w))
             for i in range(max_len - len(vec)):
                 tmp.append(0)
             expr_data.append(tmp)
@@ -49,6 +55,5 @@ def ExpressionLoader(data):
         lines = rf.readlines()
         for l in lines:
             vec = l.split()
-            tmp_dic = ['xxxx', 'data0', 'data1']
-            res_data.append([tmp_dic.index(w) for w in vec])
-    return len(voc_dic), expr_data, res_data
+            res_data.append([res_voc_dic.index(w) for w in vec])
+    return len(expr_voc_dic), len(res_voc_dic),expr_data, res_data
--- pre_process.py
+++ pre_process.py
@@ -6,9 +6,12 @@ def read_txt(file):
         data = f.readlines()
         return data
 
-def gen_txt(data, name):
-    train_expr = os.path.join('data', 'bool_expr_data', name + '_expr.txt')
-    train_res = os.path.join('data', 'bool_expr_data', name + '_res.txt')
+def gen_bool_txt(Expression_data, data, name):
+    dir_name = os.path.join('data', Expression_data)
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    train_expr = os.path.join('data', Expression_data, name + '_expr.txt')
+    train_res = os.path.join('data', Expression_data, name + '_res.txt')
     f1 = open(train_expr, 'w')
     f2 = open(train_res, 'w')
     for line in data:
@@ -28,20 +31,55 @@ def gen_txt(data, name):
         # print(new_line)
         # print(expr)
         # print(res)
-
     f1.close()
     f2.close()
-            
-            
 
-train_path = os.path.join('dataset', 'bool_expr_data', 'train.txt')
-train_data = read_txt(train_path)
-gen_txt(train_data, 'train')
+def gen_cmp_txt(Expression_data, data, name):
+    dir_name = os.path.join('data', Expression_data)
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    train_expr = os.path.join('data', Expression_data, name + '_expr.txt')
+    train_res = os.path.join('data', Expression_data, name + '_res.txt')
+    f1 = open(train_expr, 'w')
+    f2 = open(train_res, 'w')
+    for line in data:
+        new_line = line.replace('"', ' ')
+        new_line = new_line.replace(')', ' ) ')
+        new_line = new_line.replace('(', ' ( ')
+        new_line = new_line.replace(':', ' : ')
+        new_line = new_line.replace(',', ' , ')
+        new_line = re.sub('\s+', ' ', new_line)
+        new_line = new_line.replace('data 0', 'data0')
+        new_line = new_line.replace('data 1', 'data1')
+        idx = new_line.index('?=')
+        expr = new_line[1:idx-1]
+        res = new_line[idx+3:]
+        f1.write(expr + '\n')
+        f2.write(res + '\n')
+        # print(new_line)
+        # print(expr)
+        # print(res)
+    f1.close()
+    f2.close()
 
-test_path = os.path.join('dataset', 'bool_expr_data', 'test.txt')
-test_data = read_txt(test_path)
-gen_txt(test_data, 'test')
+def gen_txt(Expression_data, data, name):
+    if Expression_data == 'bool_20000':
+        gen_bool_txt(Expression_data, data, name)
+    elif Expression_data == 'bin_cmp':
+        gen_cmp_txt(Expression_data, data, name)
 
-ext_path = os.path.join('dataset', 'bool_expr_data', 'extra_test.txt')
-ext_data = read_txt(ext_path)
-gen_txt(ext_data, 'extra_test')
+def PreProcess(Expression_data):
+    train_path = os.path.join('dataset', Expression_data, 'train.txt')
+    train_data = read_txt(train_path)
+    gen_txt(Expression_data, train_data, 'train')
+
+    test_path = os.path.join('dataset', Expression_data, 'test.txt')
+    test_data = read_txt(test_path)
+    gen_txt(Expression_data, test_data, 'test')
+
+    ext_path = os.path.join('dataset', Expression_data, 'extra_test.txt')
+    ext_data = read_txt(ext_path)
+    gen_txt(Expression_data, ext_data, 'extra_test')
+
+# PreProcess('bool_20000')
+PreProcess('bin_cmp')
