d5cadb89222e124e6a0ca3e001006ae78290e738
chenxz <792146066@qq.com>
2021-12-16 20:05:18 +0800

    <400

--- count_len.py
+++ count_len.py
@@ -1,11 +1,11 @@
 import os
 
-file_path = os.path.join('data', 'bin_add_mult_expr_2e4', 'test_expr.txt')
+file_path = os.path.join('data', 'bin_add_mult_expr_2e4', 'extra_test_expr.txt')
 
 useless = []
 
-new_test_expr = os.path.join('data', 'bin_add_mult_expr_2e4', 'new_test_expr.txt')
-new_expr = open(new_test_expr, 'w')
+new_extra_test_expr = os.path.join('data', 'bin_add_mult_expr_2e4', 'new_extra_test_expr.txt')
+new_expr = open(new_extra_test_expr, 'w')
 with open(file_path, 'r') as f:
     lines = f.readlines()
     for i in range(len(lines)):
@@ -16,9 +16,9 @@ with open(file_path, 'r') as f:
             new_expr.write(lines[i])
 new_expr.close()
 
-new_res_path = os.path.join('data', 'bin_add_mult_expr_2e4', 'new_test_res.txt')
+new_res_path = os.path.join('data', 'bin_add_mult_expr_2e4', 'new_extra_test_res.txt')
 new_res = open(new_res_path, 'w')
-file_path = os.path.join('data', 'bin_add_mult_expr_2e4', 'test_res.txt')
+file_path = os.path.join('data', 'bin_add_mult_expr_2e4', 'extra_test_res.txt')
 with open(file_path, 'r') as f:
     lines = f.readlines()
     for i in range(len(lines)):
@@ -27,7 +27,7 @@ with open(file_path, 'r') as f:
 new_res.close()
 
 count = 0
-file_path = os.path.join('data', 'bin_add_mult_expr_2e4', 'new_test_expr.txt')
+file_path = os.path.join('data', 'bin_add_mult_expr_2e4', 'new_extra_test_expr.txt')
 with open(file_path, 'r') as f:
     lines = f.readlines()
     for l in lines:
